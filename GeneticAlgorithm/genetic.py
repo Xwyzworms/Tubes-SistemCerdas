3c29fb4a6aa34adee8e88c4edd3d9f05a2f9b262
XwyzWorm <pratamaazmi1@gmail.com>
2021-03-30 09:02:25 +0700

    Delete Few function , useless

--- GeneticAlgorithm/genetic.py
+++ GeneticAlgorithm/genetic.py
@@ -17,51 +17,6 @@ def generate_data(size : int = 100,coefs :int = 4) -> np.array:
     
     return np.array(x), np.array(y)
 
-def linReg(inputs : np.array , outputs : np.array) -> Dict[str,Any]:
-    """
-    Calculate SST , SSR ,RSquared and SSE
-    SST : Total Variability
-    SSR : How Well Line fits the data
-    RSquared : How Well Line Explained the data
-    Args:
-        inputs (np.array): [description]
-        outputs (np.array): [description]
-
-    Returns:
-        Dict: RSquared, Coeff,and SSE
-    """
-    info : Dict[str,Any] = {}
-    X : np.array = inputs.copy()
-    Y : np.array = outputs.copy()
-    
-    coeff = np.dot( np.linalg.pinv(np.dot(X.transpose(), X)), np.dot(X.transpose(), Y) ) 
-    Y_mean : np.float = np.mean(Y)
-    yPred : np.array = np.dot(X, coeff)
-    
-    SST : np.float = np.sum(np.array([(i - Y_mean) ** 2 for i in Y]),axis=None)
-    SSR : np.float = np.sum(np.array([(y - ypred)**2 for y,ypred in zip(Y,yPred)]),axis= None)
-    Rsquared : np.float = (1 - (SSR / SST)) * 100
-    SSE = (SSR / len(Y))
-
-    info["Rsquared"] = Rsquared
-    info["coeff" ] =  coeff
-    info["error"] = SSE
-    
-    return info
-
-def terminate_GA(best : Dict[str,Any]) -> bool:
-    """
-     Terminate the Genetic Algorithm if the RSquared > 0.98 < 0- 1 > 
-
-    Args:
-        best (Dict[str,Any]): dict Of individual Information
-
-    Returns:
-      bool  
-    """
-    if best["Rsquared"] > 98.0:
-        return True
-    return False
 
 
 class Population:
@@ -223,9 +178,8 @@ if __name__ == "__main__":
         pop.evaluate_population(x,y,10)
         pop.create_new_generation(probability_Indiv,probaiblity_gene)
         print(f"Generation {i}")
-        terminate_ga = terminate_GA(pop.bestIndividuals[0][0])
-
     
+    print(pop.bestIndividuals[0][0])
 
     #parent1 = pop.fitness(individual,x,y)
     #parent2 = pop.fitness(individual,x,y)
